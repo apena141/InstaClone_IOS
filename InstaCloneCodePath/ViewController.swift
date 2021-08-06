c8a12ae7c5409becc861a491926a4e79975a2b35
Anthony <anthony.pena0898@gmail.com>
2021-08-06 14:27:04 -0400

    Initial Commit

---
+++ InstaCloneCodePath/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneCodePath
+//
+//  Created by Anthony Pena on 8/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
